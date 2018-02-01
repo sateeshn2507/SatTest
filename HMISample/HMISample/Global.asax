46cce62ccc82fa60007626daa1115e569cfce88c
Sateesh Kumar <sateeshn2507@gmail.com>
2018-02-01 17:31:55 +0530

    New Project Created

---
+++ HMISample/HMISample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HMISample.Global" Language="C#" %>
---
+++ HMISample/HMISample/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HMISample.Startup))]
+namespace HMISample
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
